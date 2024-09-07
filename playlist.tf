164d50ec70e9a50a46d38c9fb8f0ab31ef50127a
Olivia Cesta <ocesta@uwaterloo.ca>
2024-09-07 16:09:31 -0400

    add more songs

--- playlist.tf
+++ playlist.tf
@@ -1,8 +1,11 @@
 resource "spotify_playlist" "study_music" {
     name = "Study Music"
-    tracks = ["6fxVffaTuwjgEk5h9QyRjy"]
+    tracks = ["6fxVffaTuwjgEk5h9QyRjy", "2hKdd3qO7cWr2Jo0Bcs0MA", "3Vr3zh0r7ALn8VLqCiRR10",
+              "2RiBogNRfulkNf7fVbPOrJ", "0FDzzruyVECATHXKHFs9eJ", "7cA3PnUQDlUyBBwpRozQeo",
+              "4U45aEWtQhrm8A5mxPaFZ7", "3vUn7FE3ZaQgJmkhIrIirF", "1fDtoTPDyzkNOfFIRXxsC5",
+              "3ZffCQKLFLUvYM59XKLbVm", "7hVZJGsh5fxFtwaqgJq1nf", "0W4Kpfp1w2xkY3PrV714B7",
+              "2ixsaeFioXJmMgkkbd4uj1", "6vWXl7JGlTTIHJCDCQTPjD", "6me7F0aaZjwDo6RJ5MrfBD"]
 }
-
 data "spotify_search_track" "TheFray" {
     artist = "The Fray"
 }
@@ -12,6 +15,12 @@ resource "spotify_playlist" "theFrayBestHits" {
     tracks = [data.spotify_search_track.TheFray.tracks[0].id,
               data.spotify_search_track.TheFray.tracks[1].id,
               data.spotify_search_track.TheFray.tracks[2].id,
-              data.spotify_search_track.TheFray.tracks[3].id
+              data.spotify_search_track.TheFray.tracks[3].id,
+              data.spotify_search_track.TheFray.tracks[4].id,
+              data.spotify_search_track.TheFray.tracks[5].id,
+              data.spotify_search_track.TheFray.tracks[6].id,
+              data.spotify_search_track.TheFray.tracks[7].id,
+              data.spotify_search_track.TheFray.tracks[8].id,
+              data.spotify_search_track.TheFray.tracks[9].id
     ]
 }
