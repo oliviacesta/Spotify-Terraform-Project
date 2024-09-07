b7e14822a8a0a7adf26f995f337724c32185a16b
Olivia Cesta <ocesta@uwaterloo.ca>
2024-09-07 15:42:27 -0400

    add The Fray playlist

--- playlist.tf
+++ playlist.tf
@@ -1,4 +1,17 @@
 resource "spotify_playlist" "study_music" {
-    name = "study_music"
+    name = "Study Music"
     tracks = ["6fxVffaTuwjgEk5h9QyRjy"]
+}
+
+data "spotify_search_track" "TheFray" {
+    artist = "The Fray"
+}
+
+resource "spotify_playlist" "theFrayBestHits" {
+    name = "The Fray Best Hits"
+    tracks = [data.spotify_search_track.TheFray.tracks[0].id,
+              data.spotify_search_track.TheFray.tracks[1].id,
+              data.spotify_search_track.TheFray.tracks[2].id,
+              data.spotify_search_track.TheFray.tracks[3].id
+    ]
 }
